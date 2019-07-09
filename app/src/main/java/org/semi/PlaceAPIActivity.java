6ae8b23b72047027285b8ab2f744f3e9469777e1
MinhTran <32156385+minhtran0918@users.noreply.github.com>
2019-07-09 07:10:02 +0700

    Up 8/7/2019: range all feature

--- app/src/main/java/org/semi/PlaceAPIActivity.java
+++ app/src/main/java/org/semi/PlaceAPIActivity.java
@@ -5,19 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPhotoResponse;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
 import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -41,58 +50,86 @@ public class PlaceAPIActivity extends AppCompatActivity {
             Place.Field.NAME,
             Place.Field.ADDRESS);
     AutocompleteSupportFragment places_fragment;
-    Button btn_find_current_place;
     String place_id = "";
+    TextView test_address, test_edt_likehood;
+    ImageView test_photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_api);
-        btn_find_current_place = findViewById(R.id.test_find_current_place);
-        btn_find_current_place.setOnClickListener(view -> {
-
-        });
+        test_address = findViewById(R.id.test_address);
+        test_edt_likehood = findViewById(R.id.test_likelihood);
+        test_photo = findViewById(R.id.test_photo);
+        requestPermission();
         initPlaces();
         setPlaceAutoComplete();
     }
 
-    private void getCurrentPlace() {
-        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(mPlaceFields).build();
+    private void initPlaces() {
+        Places.initialize(getApplicationContext(), getString(R.string.place_api_key));
+        mPlacesClient = Places.createClient(this);
+    }
 
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        Task<FindCurrentPlaceResponse> taskReponse = mPlacesClient.findCurrentPlace(request);
-        taskReponse.addOnCompleteListener(new OnCompleteListener<FindCurrentPlaceResponse>() {
+    private void setPlaceAutoComplete() {
+        places_fragment = (AutocompleteSupportFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.places_autocomplete_fragment);
+        places_fragment.setPlaceFields(mPlaceFields);
+        places_fragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
-                if (task.isSuccessful()) {
-                    FindCurrentPlaceResponse response = task.getResult();
+            public void onPlaceSelected(@NonNull Place place) {
+                place_id = place.getId();
+                Toast.makeText(PlaceAPIActivity.this, place.getId(), Toast.LENGTH_SHORT).show();
+            }
 
-                    Collections.sort(response.getPlaceLikelihoods(), new Comparator<PlaceLikelihood>() {
-                        @Override
-                        public int compare(PlaceLikelihood placeLikelihood, PlaceLikelihood t1) {
-                            return new Double(placeLikelihood.getLikelihood()).compareTo(t1.getLikelihood());
-                        }
-                    });
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.d("Semi", status.getStatusMessage());
+                Toast.makeText(PlaceAPIActivity.this, status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-                    Collections.reverse(response.getPlaceLikelihoods());
+    public void getCurrentPlace(View view) {
+        getCurrentPlaceTask();
+    }
 
-                    place_id = response.getPlaceLikelihoods().get(0).getPlace().getId();
-                    Log.d("Semi", "Address: " + response.getPlaceLikelihoods().get(0).getPlace().getAddress());
-                    StringBuilder sb = new StringBuilder();
-                    for (PlaceLikelihood place : response.getPlaceLikelihoods()) {
-                        sb.append(place.getPlace().getName())
-                                .append(" - Likelihoods value: ")
-                                .append("\n");
+    public void getPhotoListener(View v) {
+        getPhoto(place_id);
+    }
+
+    private void getPhoto(String place_id) {
+        FetchPlaceRequest request = FetchPlaceRequest.builder(place_id, Arrays.asList(Place.Field.PHOTO_METADATAS)).build();
+        mPlacesClient.fetchPlace(request).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+            @Override
+            public void onSuccess(FetchPlaceResponse fetchPlaceResponse) {
+                Place place = fetchPlaceResponse.getPlace();
+
+                PhotoMetadata photoMetadata = place.getPhotoMetadatas().get(0);
+
+                FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(photoMetadata).build();
+                mPlacesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
+                    @Override
+                    public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
+                        Bitmap bitmap = fetchPhotoResponse.getBitmap();
+                        Glide.with(PlaceAPIActivity.this)
+                                .load(bitmap)
+                                .placeholder(R.drawable.all_ic_loading_black_24)
+                                .error(R.drawable.ic_home_mini_store)
+                                .into(test_photo);
+
                     }
-                    Log.d("Semi", "Place: " + sb.toString());
-                }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Semi", "Fetch photo error: " + e.getMessage());
+                    }
+                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-
+                Log.d("Semi", "FetchPlaceRequest error: " + e.getMessage());
             }
         });
     }
@@ -113,27 +150,52 @@ public class PlaceAPIActivity extends AppCompatActivity {
                 }).check();
     }
 
-    private void initPlaces() {
-        Places.initialize(getApplicationContext(), getString(R.string.place_api_key));
-        mPlacesClient = Places.createClient(this);
-    }
+    private void getCurrentPlaceTask() {
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(mPlaceFields).build();
 
-    private void setPlaceAutoComplete() {
-        places_fragment = (AutocompleteSupportFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.places_autocomplete_fragment);
-        places_fragment.setPlaceFields(mPlaceFields);
-        places_fragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "chưa cấp quyền", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Task<FindCurrentPlaceResponse> taskReponse = mPlacesClient.findCurrentPlace(request);
+        taskReponse.addOnCompleteListener(new OnCompleteListener<FindCurrentPlaceResponse>() {
             @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                Toast.makeText(PlaceAPIActivity.this, place.getId(), Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
+                if (task.isSuccessful()) {
+                    FindCurrentPlaceResponse response = task.getResult();
+                    //If success -> Placelihood return list of likelihood place
+
+                    /*Collections.sort(response.getPlaceLikelihoods(), new Comparator<PlaceLikelihood>() {
+                        @Override
+                        public int compare(PlaceLikelihood placeLikelihood, PlaceLikelihood t1) {
+                            return new Double(placeLikelihood.getLikelihood()).compareTo(t1.getLikelihood());
+                        }
+                    });
+
+                    Collections.reverse(response.getPlaceLikelihoods());*/
+
+                    place_id = response.getPlaceLikelihoods().get(0).getPlace().getId();
+                    Log.d("Semi", "Address: " + response.getPlaceLikelihoods().get(0).getPlace().getAddress());
+                    StringBuilder sb = new StringBuilder();
+                    for (PlaceLikelihood place : response.getPlaceLikelihoods()) {
+                        sb.append("Name place: ").append(place.getPlace().getName()).append("\n");
+                               // .append("type ").append(place.getPlace().getTypes().size()).append("\n")
+                                //.append("lat ").append(place.getPlace().getLatLng().latitude).append("\n")
+                                //.append("lng ").append(place.getPlace().getLatLng().longitude).append("\n")
+                                //.append("open ").append(place.getPlace().getOpeningHours().toString()).append("\n")
+                                //.append("name ").append(place.getPlace().getName()).append("\n");
+                    }
+                    test_edt_likehood.setText(sb.toString());
+                    Log.d("Semi", "Place: " + sb.toString());
+                }
             }
-
+        }).addOnFailureListener(new OnFailureListener() {
             @Override
-            public void onError(@NonNull Status status) {
-                Log.d("Semi", status.getStatusMessage());
-                Toast.makeText(PlaceAPIActivity.this, status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            public void onFailure(@NonNull Exception e) {
+
             }
         });
     }
+
 
 }
